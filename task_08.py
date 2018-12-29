63c39c59e0bbee21ccb95b8bbf6a0ebea5059a31
Thomas Power <thomaspwr@gmail.com>
2018-12-29 11:27:57 +0000

    Adds task_08 and task_09.

---
+++ task_08.py
@@ -0,0 +1,10 @@
+"""
+Given the two lists below, print out the full names of each of
+the 3 people, one per line.
+
+Do NOT use any flow-control statements.
+> https://www.oreilly.com/library/view/python-in-a/0596001886/ch04s09.html
+"""
+
+first_names = ["Jo", "Thomas", "Little"]
+last_names = ["Ball", "Power", "Spark"]
---
+++ task_09.py
@@ -0,0 +1,7 @@
+"""
+Repeat the challenge from task_08, but this time you MUST use
+flow-control statements. The output should be identical.
+"""
+
+first_names = ["Jo", "Thomas", "Little"]
+last_names = ["Ball", "Power", "Spark"]
